d60e7d820c4465b699fbe1c159701edb3c55ca24
Giorgio <imatesiu@gmail.com>
2017-11-20 14:43:05 +0100

    improved olv sender

--- senderDocOLV.py
+++ senderDocOLV.py
@@ -1,12 +1,35 @@
 import requests
 import time
+import xml.etree.ElementTree
+from datetime import datetime
+from datetime import timedelta
 
 set_ip_apparato = "192.168.0.54"
+
+def xmltodate(xml):
+	root = ET.fromstring(getdate)
+	a = (root.findall(".//extraInfo"))
+	text = a[0].text
+	datet = datetime.strptime(text, '%d%m%Y%H%M%S')
+	return datet
+	#print res
+	
+def dateplus(date):
+	end_date = date + timedelta(days=1)
+	return end_date.strftime('%d%m%Y%H%M%S')
+	
+	
+def createnewdata(xml):
+	da = xmltodate(xml)
+	r = dateplus(da)
+	data4 = "<?xml version=\"1.0\" encoding=\"utf-8\"?><soap:Envelope xmlns:soap=\"http://www.w3.org/2001/12/soap-envelope\"><soap:Body><SetEcrDate><SETDATE><DATE>"+r+"</DATE><TIME>010000</TIME></SETDATE></SetEcrDate></soap:Body></soap:Envelope>"
+	return data4 
 
 def send_post(content):
 	response = requests.post('http://'+set_ip_apparato+':80/oli_webservice.cgi',data=content,headers={"Content-Type": "text/xml"})
 	print response.text
 	assert response.status_code == 200
+	return xml
 
 documentocommerciale = "<?xml version=\"1.0\" encoding=\"utf-8\"?><soap:Envelope xmlns:soap=\"http://www.w3.org/2001/12/soap-envelope\"><soap:Body><EcrTickets><CMD>...<E_SALE>..<OPTYPE>1</OPTYPE>....<BARCODETYPE></BARCODETYPE>..<NUMBER>1</NUMBER>..<LISTPRICE>1</LISTPRICE>..<SALETYPE>1</SALETYPE>..<DESCRIPTION></DESCRIPTION>..<AMOUNT>1</AMOUNT>..<QUANTITY_1></QUANTITY_1>..<QUANTITY_2></QUANTITY_2>..<QUANTITY_3></QUANTITY_3>..<QUANTITY_4></QUANTITY_4>..<M_QUANTITY></M_QUANTITY>..<S_QUANTITY></S_QUANTITY>..<RAEETYPE></RAEETYPE>..<RAEEVALUE></RAEEVALUE>.</E_SALE></CMD><CMD>...<E_PAYMENT>..<P_TYPE>1</P_TYPE>...<NUMBER>1</NUMBER>...<DESCRIPTION>PAGAMENTO</DESCRIPTION>...<AMOUNT></AMOUNT>....<CUSTOMERACCOUNT></CUSTOMERACCOUNT>...</E_PAYMENT></CMD></EcrTickets></soap:Body></soap:Envelope>"
 
@@ -22,15 +45,14 @@ def create_data(day):
 	data4 = "<?xml version=\"1.0\" encoding=\"utf-8\"?><soap:Envelope xmlns:soap=\"http://www.w3.org/2001/12/soap-envelope\"><soap:Body><SetEcrDate><SETDATE><DATE>"+da+"112017</DATE><TIME>010000</TIME></SETDATE></SetEcrDate></soap:Body></soap:Envelope>"
 	return data4
 
-day = 12
+
 num = 2
 while True:
 	if num == 1 or num == 80:
-			send_post(changedate)
-			day += 1
-			se = create_data(day)
-			send_post(se)
-			send_post(se)
+			xml = send_post(changedate)
+			dmsg = createnewdata(xml)
+			send_post(dmsg)
+			send_post(dmsg)
 			num = 2
 	send_post(documentocommerciale)
 	send_post(zfiscale)
