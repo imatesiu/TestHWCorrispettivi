3758655ec95c78a7745e8ff534118b1aa8598d8a
ssec <ssec@MacBook-Pro-di-ssec.local>
2017-11-20 14:06:55 +0100

    sender OLV improved

--- senderDocOLV.py
+++ senderDocOLV.py
@@ -1,7 +1,7 @@
 import requests
 import time
 
-set_ip_apparato = "192.168.1.110"
+set_ip_apparato = "192.168.0.54"
 
 def send_post(content):
 	response = requests.post('http://'+set_ip_apparato+':80/oli_webservice.cgi',data=content,headers={"Content-Type": "text/xml"})
@@ -14,18 +14,27 @@ zfiscale = "<?xml version=\"1.0\" encoding=\"utf-8\"?><soap:Envelope xmlns:soap=
 
 changedate = "<?xml version=\"1.0\" encoding=\"utf-8\"?><soap:Envelope xmlns:soap=\"http://www.w3.org/2001/12/soap-envelope\"><soap:Body><EcrTickets><CMD><GETDATE></GETDATE></CMD></EcrTickets></soap:Body></soap:Envelope>"
 
-day = 7
+day = 6
 data2 = "<?xml version=\"1.0\" encoding=\"utf-8\"?><soap:Envelope xmlns:soap=\"http://www.w3.org/2001/12/soap-envelope\"><soap:Body><SetEcrDate><SETDATE><DATE>08102017</DATE><TIME>010000</TIME></SETDATE></SetEcrDate></soap:Body></soap:Envelope>"
 
+def create_data(day):
+	da = str(day).zfill(2)
+	data4 = "<?xml version=\"1.0\" encoding=\"utf-8\"?><soap:Envelope xmlns:soap=\"http://www.w3.org/2001/12/soap-envelope\"><soap:Body><SetEcrDate><SETDATE><DATE>"+da+"112017</DATE><TIME>010000</TIME></SETDATE></SetEcrDate></soap:Body></soap:Envelope>"
+	return data4
 
+day = 12
 num = 2
 while True:
-	if num == 1 or num == 70:
+	if num == 1 or num == 80:
 			send_post(changedate)
-			send_post(data2)
-			send_post(data2)
+			day += 1
+			se = create_data(day)
+			send_post(se)
+			send_post(se)
 			num = 2
 	send_post(documentocommerciale)
 	send_post(zfiscale)
+	print num 
+	print day
 	time.sleep(5)
 	num +=1
