84811ffe85a080cd4b8293f9ec187198de3ab934
Giorgio <imatesiu@gmail.com>
2018-11-18 12:20:12 +0100

    curl flle updated

--- src/main/java/isti/cnr/sse/rest/impl/Utility.java
+++ src/main/java/isti/cnr/sse/rest/impl/Utility.java
@@ -212,6 +212,9 @@ public class Utility {
 						for (int j = 0; i.hasNext(); j++) {
 							boolean refValid = ((Reference) i.next()).validate(valContext);
 							System.out.println("ref[" + j + "] validity status: " + refValid);
+							if(refValid==true){
+								validFlag = true;
+							}
 						}
 					}
 				} else {
