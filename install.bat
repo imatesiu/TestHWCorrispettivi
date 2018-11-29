e046cdbf62d0448e726289dba974eb326ffeff2c
Giorgio <imatesiu@gmail.com>
2018-11-29 18:46:48 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -3,8 +3,8 @@ powershell -Command "(New-Object Net.WebClient).DownloadFile('https://ssec.isti.
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://ssec.isti.cnr.it/download/apache-maven-3.6.0-bin.zip', 'apache-maven-3.6.0-bin.zip')"
 
 powershell -Command "Expand-Archive apache-maven-3.6.0-bin.zip"
-powershell -Command "Start-Process '.\git.exe' "/S""
-powershell -Command "Start-Process '.\jdk-8u191-windows-x64.exe' "
+powershell -Command "Start-Process '.\git.exe' '/S'"
+powershell -Command "Start-Process '.\jdk-8u191-windows-x64.exe' /s"
 
 powershell -Command "$theCurrentPath=(Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path; $fullpath = (Get-Item -Path '.\').FullName; $fullpath += '\apache-maven-3.6.0-bin\apache-maven-3.6.0\bin'; $theUpdatedPath=$theCurrentPath+';C:\Program Files\Java\jdk1.8.0_191\jre\bin\;'+$fullpath; Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH –Value $theUpdatedPath; "
 
