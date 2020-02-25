f23440da7a427bed61a539135e0604af2bae5c88
Giorgio <imatesiu@gmail.com>
2020-02-25 19:30:20 +0100

    ssl improved

--- run.bat
+++ run.bat
@@ -1,2 +1,3 @@
-mvn jetty:run 
+mvn jetty:run -Djdk.tls.ephemeralDHKeySize=2048 -Djdk.tls.server.defaultDHEParameters=2048
+
 
--- run_debug.sh
+++ run_debug.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 mvn clean
 mvn install
-mvn jetty:run -Dorg.eclipse.jetty.util.log.class=org.eclipse.jetty.util.log.StdErrLog  -Dorg.eclipse.jetty.LEVEL=DEBUG
+mvn jetty:run -Dorg.eclipse.jetty.util.log.class=org.eclipse.jetty.util.log.StdErrLog  -Dorg.eclipse.jetty.LEVEL=DEBUG -Djdk.tls.ephemeralDHKeySize=2048 -Djdk.tls.server.defaultDHEParameters=2048
 
 # tomcat7:run
