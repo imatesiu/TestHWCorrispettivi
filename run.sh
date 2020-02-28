cc88628d07ddf6804cef6e98c41efa153c78b12f
Giorgio <imatesiu@gmail.com>
2020-02-28 18:42:30 +0100

    run fixed

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #mvn keytool:clean keytool:generateKeyPair keytool:exportCertificate
-mvn jetty:run -Djdk.tls.ephemeralDHKeySize=2048 -Djdk.tls.server.defaultDHEParameters=2048
+mvn clean jetty:run -Djdk.tls.ephemeralDHKeySize=2048 -Djdk.tls.server.defaultDHEParameters=2048
 
 
 # tomcat7:run
